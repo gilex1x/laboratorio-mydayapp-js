38205b02d4cca662f7009bea3b1f5e0e31330f7c
gilberto <gilberto.santamaria@apriorins.com>
2023-10-17 22:38:10 -0500

    [ADD]Eneabled to create news todos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./css/base.css";
+import { sayHello, createTodo } from "./js/utils";
 
-import { sayHello } from "./js/utils";
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
+const createInput = document.getElementById('new-todo');
+
+createInput.addEventListener('keypress', (event) => {
+    if (event.key == 'Enter') {
+        console.log(createInput.value);
+        createTodo(createInput.value)
+    }
+});
 
 console.log(sayHello("Hello"));
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,37 @@
 export const sayHello = (text) => {
   return text;
 };
+
+export const createTodo = (todo) => {
+  let todoItem = {
+    title: todo,
+    id: '',
+    completed: false
+  };
+  window.localStorage.setItem('todos', JSON.stringify([todoItem]));
+  //Ad to localStorage
+  drawItems();
+}
+
+const drawItems = () => {
+  //get items from localStorage
+  let items = window.localStorage.getItem('todos');
+  let itemsList = JSON.parse(items);
+  let drawItems = [];
+  itemsList.forEach(element => {
+    let listElement = document.createElement('li');
+    listElement.className = `${element.completed ? 'completed' : null}`;
+    listElement.innerHTML = `
+      <div class="view">
+        <input class="toggle" type="checkbox" ${element.completed ? 'cheked' : null} />
+        <label>${element.title}</label>
+        <button class="destroy" id="${element.id ? element.id : '0'}"></button>
+      </div>
+      <input class="edit"  value="Learn JavaScript" />`;
+    drawItems.push(listElement);
+  });
+
+  let container = document.getElementById('todo-list-container');
+  container.append(...drawItems);
+
+}
